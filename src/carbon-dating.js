65658fd7f7b6dc4cadde697e1326f6b20308fe4c
Jalolov Sultonali <jsultonali1993@mail.ru>
2020-02-17 19:54:38 +0500

    feat: finish task carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,12 +2,21 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
-  if (typeof sampleActivity !== "string" || isNaN(+sampleActivity)) {
+  let inputNumber = parseFloat(sampleActivity);
+  if (typeof(sampleActivity) !== "string" ||
+      isNaN(inputNumber) ||
+      sampleActivity === '' ||
+      sampleActivity === null ||
+      sampleActivity === undefined ||
+      +sampleActivity > 15 ||
+      +sampleActivity <= 0
+  ) {
     return false;
   }
-  let k = (+sampleActivity / MODERN_ACTIVITY) / HALF_LIFE_PERIOD;
 
-  let t = Math.log(MODERN_ACTIVITY / +sampleActivity) / k;
-  return Math.round(t);
+  let k = (0.693) / HALF_LIFE_PERIOD;
+
+  let t = Math.log(MODERN_ACTIVITY / inputNumber) / (k);
+  return +Math.ceil(t);
 
 };
