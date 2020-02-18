8b267148f1ee82df1caea854d42897feef8d24f9
Jalolov Sultonali <jsultonali1993@mail.ru>
2020-02-18 11:13:25 +0500

    feat: finish task recursive-depth

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,6 +1,18 @@
+
+
 module.exports = class DepthCalculator {
-    calculateDepth(/* arr */) {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    calculateDepth(arr) {
+        if (Array.isArray(arr)) {
+            if (arr.length === 0) { return 1; }
+            //console.log('true',arr, count);
+            return 1 + Math.max(...arr.map(t => this.calculateDepth(t)));
+
+        } else {
+            //console.log('false');
+            return 0;
+        }
     }
 };
+
+
+
